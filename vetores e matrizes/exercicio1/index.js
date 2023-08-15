974b658a722e539175d9f1d824ef88359e65fa9a
Lucas Magalhães <103212436+Olderc4t@users.noreply.github.com>
2023-08-14 22:17:19 -0300

    v1
    Estudar vetores para melhor entendimento em sua usabilidade nos exercicios

---
+++ vetores e matrizes/exercicio1/index.js
@@ -0,0 +1,15 @@
+const readline = require('readline-sync');
+
+/*Crie um programa que receba 3 números do usuário, coloque-os em um 
+array, depois exiba tal array. Modifique os elementos do array de modo a 
+sequência de números ficar do contrário.
+Ou seja, se digitou: 1,2,3
+Vai aparecer: 3,2,1*/
+
+let v = new Array(2)
+
+for(let i = 0; i <= 0; i++){
+    vetor[i] 
+    Number(readline.question("Escreva 3 numeros : "));
+    console.log(v)
+}
